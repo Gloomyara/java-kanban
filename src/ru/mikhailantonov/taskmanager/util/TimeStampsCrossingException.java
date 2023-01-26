84a46c3feca9c24d2319233808b7cba8f7ffbcb6
Gloomyara <gloomyara@gmail.com>
2023-01-27 00:21:48 +0400

    fix : подправить проброс исключений

--- src/ru/mikhailantonov/taskmanager/util/TimeStampsCrossingException.java
+++ src/ru/mikhailantonov/taskmanager/util/TimeStampsCrossingException.java
@@ -4,7 +4,7 @@ package ru.mikhailantonov.taskmanager.util;
 /**
  * собственное исключение для обработки ошибок, выбрасывается при обнаружении пересечений временных меток
  */
-public class TimeStampsCrossingException extends Exception {
+public class TimeStampsCrossingException extends RuntimeException {
     public TimeStampsCrossingException(final String message) {
         super(message);
     }
